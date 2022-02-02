ad447887adc777c9ebe1f61213ccf7d1656d503e
samuel-davis95 <samadavis123@gmail.com>
2022-02-01 21:40:23 -0500

    Created EntityManager class

---
+++ SuperNova/EntityManager.h
@@ -0,0 +1,20 @@
+#include <cstdint>
+#include <vector>
+#pragma once
+
+//Define an entity as an ID
+using Entity = std::uint32_t;
+//Max number of entities
+//const Entity MAX_ENTITIES = 5000;
+
+
+class EntityManager
+{
+private:
+	std::vector<Entity> entites;
+public:
+	EntityManager() {}
+
+	Entity CreateEntity();
+};
+
