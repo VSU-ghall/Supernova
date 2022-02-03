a04c712a2e94eac330efc3838746c9552f102b00
samuel-davis95 <samadavis123@gmail.com>
2022-02-02 22:33:43 -0500

    Added clause to make EntityManager a friend class

--- SuperNova/Entity.h
+++ SuperNova/Entity.h
@@ -2,12 +2,18 @@
 #include <string>
 class Entity
 {
+
 private:
+
+	//Since the constructor is private EntityManager class needs this to create Entities
+	friend class EntityManager;
+
 	size_t id = 0; //unique id of the entity
 	std::string tag = "default"; //tag to allow similar entities to be gathered
 	bool isActive = true; 
 
 	Entity(const size_t& id, const std::string& tag);
+
 public:
 	
 	bool getIsActive();
