43a52dce6261e99d1724b4d19e35489816476fed
samuel-davis95 <samadavis123@gmail.com>
2022-02-02 21:39:41 -0500

    Added code for methods

--- SuperNova/Entity.cpp
+++ SuperNova/Entity.cpp
@@ -1,1 +1,21 @@
 #include "Entity.h"
+
+Entity::Entity(const size_t& id, const std::string& tag)
+	:id(id), tag(tag)
+{}
+
+bool Entity::getIsActive() {
+	return isActive;
+}
+
+std::string& Entity::getTag() {
+	return tag;
+}
+
+size_t& Entity::getId() {
+	return id;
+}
+
+void Entity::destroy() {
+	isActive = false;
+}
