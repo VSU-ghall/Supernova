4fd9d9ba25dd0e823e62eefd8d1a0f7ea7f2fb80
VSU-ghall <69879222+VSU-ghall@users.noreply.github.com>
2022-02-05 15:10:00 -0500

    Added viewport to game
    Fixed resizing/stretching issues by adding a viewport

--- SuperNova/src/main.cpp
+++ SuperNova/src/main.cpp
@@ -8,13 +8,17 @@ void init();
 void draw();
 void update();
 void drawGrid();
+sf::View getViewport(float width, float height);
 
 Player player;
 TileMap map;
-const int tileSize = 64, levelWidth = 20, levelHeight = 12;
+const int tileSize = 64;
+int levelWidth = 20, levelHeight = 12;
+float windowWidth = tileSize * levelWidth, windowHeight = tileSize * levelHeight;
 
 // creates global window
-sf::RenderWindow window(sf::VideoMode(levelWidth * tileSize, levelHeight * tileSize), "SuperNova");
+sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "SuperNova");
+sf::View view;
 
 int main(int argc, char **argv)
 {
@@ -35,7 +39,7 @@ int main(int argc, char **argv)
 		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 
 	};
-
+	
 	// create the tilemap from the level definition
 	
 	if (!map.load("src/img/testTileSet.png", sf::Vector2u(64,64), level, levelWidth, levelHeight))
@@ -52,9 +56,13 @@ int main(int argc, char **argv)
 		while (window.pollEvent(event)) {
 
 			// event triggered when window is closed
-			if (event.type == sf::Event::Closed || event.type == sf::Keyboard::Escape) {
+			if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
 				window.close();
 			}
+
+			// sets viewport when window is resized
+			if (event.type == sf::Event::Resized)
+				view = getViewport(event.size.width, event.size.height);
 
 		}
 
@@ -72,6 +80,10 @@ int main(int argc, char **argv)
 //	Initializes the game components
 //
 void init() {
+	view.setSize(windowWidth, windowHeight);
+	view.setCenter(view.getSize().x / 2, view.getSize().y / 2);
+	view = getViewport(windowWidth, windowHeight);
+
 	window.setFramerateLimit(60);
 	player.init();
 }
@@ -82,6 +94,7 @@ void init() {
 void draw() {
 	window.clear();
 	
+	window.setView(view);
 	window.draw(map);
 
 	drawGrid();
@@ -98,6 +111,9 @@ void update() {
 	player.update();
 }
 
+//
+// Draws grid for development/testing purposes
+//
 void drawGrid() {
 	for (int x = 0; x <= window.getSize().x; x = x + tileSize) {
 		sf::VertexArray lines(sf::LinesStrip, 2);
@@ -118,4 +134,37 @@ void drawGrid() {
 
 		window.draw(lines);
 	}
+}
+
+//
+// Sets viewport to keep game aspect ratio
+// ** Adapted from official SFML wiki (src: https://github.com/SFML/SFML/wiki/Source%3A-Letterbox-effect-using-a-view)
+//
+sf::View getViewport(float width, float height) {
+	float windowRatio = width / height;
+	float viewRatio = windowWidth / windowHeight;
+	float sizeX = 1;
+	float sizeY = 1;
+	float posX = 0;
+	float posY = 0;
+
+	bool horizontalSpacing = true;
+	if (windowRatio < viewRatio)
+		horizontalSpacing = false;
+
+	// If horizontalSpacing is true, the black bars will appear on the left and right side.
+	// Otherwise, the black bars will appear on the top and bottom.
+
+	if (horizontalSpacing) {
+		sizeX = viewRatio / windowRatio;
+		posX = (1 - sizeX) / 2.f;
+	}
+	else {
+		sizeY = windowRatio / viewRatio;
+		posY = (1 - sizeY) / 2.f;
+	}
+
+	view.setViewport(sf::FloatRect(posX, posY, sizeX, sizeY));
+
+	return view;
 }
