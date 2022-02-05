865125c2fe9c4c0aaa76cb2a95feef8c372e4d3b
samuel-davis95 <samadavis123@gmail.com>
2022-02-05 14:27:05 -0500

    Commented code

--- SuperNova/src/Entity.h
+++ SuperNova/src/Entity.h
@@ -1,5 +1,10 @@
 #pragma once
 #include <string>
+
+/*
+	Entiy class idea originated from the following lecture: https://www.youtube.com/watch?v=4_DKdliZv_4&list=PL_xRyXins848jkwC9Coy7B4N5XTOnQZzz&index=5
+*/
+
 class Entity
 {
 
@@ -10,15 +15,15 @@ private:
 
 	size_t id = 0; //unique id of the entity
 	std::string tag = "default"; //tag to allow similar entities to be gathered
-	bool isActive = true; 
+	bool isActive = true; //true if the entity is alive false if entity is dead
 
-	Entity(const size_t& id, const std::string& tag);
+	Entity(const size_t& id, const std::string& tag); //constructor requries a unique unsigned int as id and tag
 
 public:
 	
 	bool getIsActive();
 	std::string& getTag();
 	size_t getId();
-	void destroy();
+	void destroy(); //sets isActive to false
 };
 
