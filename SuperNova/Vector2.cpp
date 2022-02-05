48dcda9aff9d8b5c360106a74785e9c788b15122
samuel-davis95 <samadavis123@gmail.com>
2022-02-04 21:44:36 -0500

    Implemented methods for Vector2 class

--- SuperNova/Vector2.cpp
+++ SuperNova/Vector2.cpp
@@ -1,1 +1,58 @@
 #include "Vector2.h"
+#include <math.h>
+
+Vector2::Vector2() {}
+
+Vector2::Vector2(float x, float y)
+	:x(x), y(y)
+{}
+
+Vector2 Vector2::operator + (Vector2 v) {
+	return Vector2(x + v.x, y + v.y);
+}
+
+Vector2 Vector2::operator - (Vector2 v) {
+	return Vector2(x - v.x, y - v.y);
+}
+
+Vector2 Vector2::operator * (Vector2 v) {
+	return Vector2(x * v.x, y * v.y);
+}
+
+Vector2 Vector2::operator / (float scale) {
+	return Vector2(x / scale, y / scale);
+}
+
+bool Vector2::operator != (Vector2 v) {
+	if (v.x != x || v.y != y) {
+		return false;
+	}
+	return true;
+}
+
+bool Vector2::operator == (Vector2 v) {
+	if (v.x == x && v.y == y) {
+		return true;
+	}
+	return false;
+}
+
+Vector2 Vector2::operator += (Vector2 v) {
+	return Vector2(x + v.x, y + v.y);
+}
+
+Vector2 Vector2::operator *= (Vector2 v) {
+	return Vector2(x * v.x, y * v.y);
+}
+
+Vector2 Vector2::operator -= (Vector2 v) {
+	return Vector2(x - v.x, y - v.y);
+}
+
+Vector2::Vector2::scale(float value) {
+	return Vector2(x * scale, y * scale);
+}
+
+double Vector2::length(Vector2 v) {
+	return sqrt(pow(x, 2) + pow(y, 2));
+}
