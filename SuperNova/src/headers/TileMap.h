c7a398e52d5bd3f706587b171add30ece6c553e7
samuel-davis95 <samadavis123@gmail.com>
2022-03-23 22:32:41 -0400

    Updated instance variable to be private

--- SuperNova/src/headers/TileMap.h
+++ SuperNova/src/headers/TileMap.h
@@ -10,10 +10,10 @@
 class TileMap : public sf::Drawable, public sf::Transformable
 {
 public:
-	std::vector<std::vector<int>> colMap;
 	bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height);
 	std::vector<std::vector<int>> loadColMap(const int* tiles, unsigned int width, unsigned int height);
 private:
+	std::vector<std::vector<int>> colMap;
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 	sf::VertexArray m_vertices;
 	sf::Texture m_tileset;
