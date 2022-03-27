ea96f58d9c8f8c4e95ac0fc3ca11944375e71178
samuel-davis95 <samadavis123@gmail.com>
2022-03-26 21:54:57 -0400

    Added vector to level struct to hold enemies

--- SuperNova/src/headers/LevelManager.h
+++ SuperNova/src/headers/LevelManager.h
@@ -4,6 +4,7 @@
 #include <headers/TileMap.h>
 #include <headers/Sprite.h>
 #include <vector>
+#include <headers/Entity.h>
 
 static class LevelManager
 {
@@ -21,9 +22,10 @@ static class LevelManager
 			int transitionTile;
 			int collisionTile;
 			int emptyTile;
+			std::vector<Entity> enemies;
 		} testLevel = {this}, 
-			level1 = { this, new Sprite("src/resources/space_background_sprite_sheet.png", true, true, 4, 1280, 768, 1.0f, 1000), true },
-			level2 = {this, new Sprite("src/resources/waterfall_background.png", true, false, 16, 350, 210, 4.57f, 500), true, 25, 17 },
+			level1 = { this, new Sprite("src/resources/space_background_sprite_sheet.png", true, true, 4, 1280, 768, 1.0f, 1000), true},
+			level2 = {this, new Sprite("src/resources/waterfall_background.png", true, false, 16, 350, 210, 4.57f, 500), true, 25, 17},
 			currentLevel = {this};
 
 		LevelManager();
